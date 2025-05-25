4d157ac666f9ea8ca8002f419a495f25d8123286
Henrique Peixoto <107963741+Pxt77@users.noreply.github.com>
2025-05-25 20:42:45 +0000

    rwk

--- implementacao/backlds3/src/main/java/br/com/lsd/backlds3/repositories/AlunoRepository.java
+++ implementacao/backlds3/src/main/java/br/com/lsd/backlds3/repositories/AlunoRepository.java
@@ -1,14 +1,12 @@
 package br.com.lsd.backlds3.repositories;
 
+import br.com.lsd.backlds3.models.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.lsd.backlds3.models.Aluno;
+import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-
+    Optional<Aluno> findByEmail(String email);
 }
-
-Optional<Aluno> findByEmail(String email);
-
